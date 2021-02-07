90df2afb11e78218ae5eb308a4064f599df6dc00
ChanceTheHacker <ertyjunior@gmail.com>
2021-02-06 20:18:31 -0500

    heroku db ssl so annoying

--- src/database/index.js
+++ src/database/index.js
@@ -17,7 +17,7 @@ exports.start = async function () {
 
 this.client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgresql://postgres:@localhost:5432/koaapi',
-    ssl: process.env.DATABASE_URL ? true : false
+    ssl: {rejectUnauthorized: false}
 });
 
 this.client.connect();
